acca461a3e1d9ca063942a9ab3a72f103165de4b
iykHvfIvfR <b1827154@trbvn.com>
2016-12-22 10:36:06 +0000

    Update to work on Devoted
    Now handles snitch names added in https://github.com/DevotedMC/JukeAlert/commit/fb346a04c78012871a4557ede6503832dde79c1e
    Should fail horribly on servers which use any older versions of JA.
    
    On a side note...
    
    I have no idea why it broke in the first place, or more accurately, I
    don't understand why it ever worked. It broke very recently but it does
    not appear that the source for Devoted's JukeAlert has changed at all.
    Additionally, looking at JukeAlert's code, the lines were always sent by
    the server as one message, so how could the regex could have ever
    possibly matched it?
    
    Also `.getUnformattedText()` doesn't seem to work?

--- src/main/java/com/github/scuwr/snitchvisualizer/handlers/SVChatHandler.java
+++ src/main/java/com/github/scuwr/snitchvisualizer/handlers/SVChatHandler.java
@@ -9,12 +9,18 @@ import java.util.regex.Pattern;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.google.gson.JsonParser;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import com.github.scuwr.snitchvisualizer.SV;
 import com.github.scuwr.snitchvisualizer.classobjects.Block;
 import com.github.scuwr.snitchvisualizer.classobjects.Snitch;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -36,68 +42,123 @@ public class SVChatHandler {
 	public static ArrayList<Block> tempList;
 
 	private static Logger logger = LogManager.getLogger("SnitchVisualizer");
-	private static Pattern snitchMessage = Pattern.compile("\\s*([^\\s]*?)\\s*\\[([-\\d]*)\\s([-\\d]*)\\s([-\\d]*)\\]\\s*(\\d*.\\d\\d)?\\s*([^\\s]*)\\s*");
+	private static Pattern snitchMessage = Pattern.compile(
+			"\\s*World:\\s*([^\\s]*)" +
+			"\\s*Location:\\s*\\[([-\\d]*)\\s*([-\\d]*)\\s*([-\\d]*)\\]" +
+			"\\s*Hours to cull:\\s*(\\d*.\\d\\d)?" +
+			"\\s*Group:\\s*([^\\s]*)" +
+			"\\s*Name:\\s*([^\\s]*)");
 
 	@SubscribeEvent
-	public void onChat(ClientChatReceivedEvent event) {
-		if (event != null && event.getMessage() != null) {
-			String msg = event.getMessage().getUnformattedText();
-			if (msg == null) {
-				return;
-			}
+	public void onClientChat(ClientChatReceivedEvent event) {
+		if (event == null || event.getMessage() == null) {
+			return;
+		}
+		String msg = event.getMessage().getUnformattedText();
+		if (msg == null) {
+			return;
+		}
+		String jsonMsg = ITextComponent.Serializer.componentToJson(event.getMessage());
+		jsonMsg = parseJalistJSON(jsonMsg);
+		if (jsonMsg != null) {
+			handleSnitchChatJsonMsg(jsonMsg);
+		} else {
+			handleSnitchChatMsg(msg);
+		}
+	}
 
-			Matcher isSM = SVChatHandler.snitchMessage.matcher(msg);
+	private String parseJalistJSON(String json) {
+		JsonElement jelement = new JsonParser().parse(json);
+		JsonObject jobject = jelement.getAsJsonObject();
+		if (!jobject.has("extra")) {
+			return null;
+		}
+		JsonArray jarray = jobject.getAsJsonArray("extra");
+		jobject = jarray.get(0).getAsJsonObject();
+		if (!jobject.has("extra")) {
+			return null;
+		}
+		jarray = jobject.getAsJsonArray("extra");
+		String result = "";
+		for (JsonElement line : jarray) {
+			jobject = line.getAsJsonObject();
+			if (!jobject.has("hoverEvent")) {
+				continue;
+			}
+			jobject = jobject.getAsJsonObject("hoverEvent");
+			if (!jobject.has("value")) {
+				continue;
+			}
+			jobject = jobject.getAsJsonObject("value");
+			if (!jobject.has("text")) {
+				continue;
+			}
+			String text = jobject.get("text").toString();
+			text = text.replaceAll("\\\\n", " ").replaceAll("^\"+", "").replaceAll("\"+$", "");
+			result += text + "\n";
+		}
+		return result;
+	}
+
+	private void handleSnitchChatJsonMsg(String jsonMsg) {
+		String lines[] = jsonMsg.split("\\n");
+		for (String line : lines) {
+			Matcher isSM = SVChatHandler.snitchMessage.matcher(line);
 			if (isSM.matches()) {
-				parseSnitch(msg, isSM);
-			} else if (msg.contains("*") && msg.contains("snitch at")) {
-				// parseSnitch(msg)
-			} else if (msg.contains("Used") || msg.contains("Block Break") || msg.contains("Block Place")) {
-				// render block place/break/use
-				parseBlock(msg);
-			} else if (msg.contains("Entry")) {
-				if (snitchReport)
-					parseEntry(msg);
-			} else if (msg.contains("Snitch Log for") || msg.contains("Page 1 is empty for snitch")) {
-				// export jainfo to csv
-				try {
-					String name = parseSnitchName(msg);
-					if (SVPlayerHandler.updateSnitchName) {
-						SVPlayerHandler.updateSnitchName = false; // done!
-					}
-					SVFileIOHandler.saveList();
-
-					if (snitchReport) {
-						if (snitchReportName.equals("")) {
-							snitchReportName = name;
-						}
-						if (!snitchReportName.equals(name)) {
-							snitchReport = false;
-							SVFileIOHandler.saveSnitchReport(snitchReportName);
-							snitchReportName = "";
-						}
-					}
-				} catch (Exception e) {
-					logger.error("Exception encountered while parsing snitch logs", e);
-
-					try {
-						Minecraft.getMinecraft().thePlayer.addChatMessage(new TextComponentString("Check logs; " +
-								"non-fatal error while parsing snitch logs"));
-					} catch (Exception f) {
-						logger.error("Couldn't notify player of error", f);
-					}
-				}
-			} else if (msg.contains("Unknown command") || msg.contains(" is empty") || msg.contains("You do not own any snitches nearby!")) {
-				jalistIndex = 1;
-				jainfoIndex = 1;
-				updateSnitchList = false;
-				if (snitchReport) {
-					snitchReport = false;
-					SVFileIOHandler.saveSnitchReport(snitchReportName);
-					snitchReportName = "";
-				}
-			} else if (msg.contains("TPS from last 1m, 5m, 15m:")) {
-				ParseTPS(msg);
+				parseSnitch(line, isSM);
 			}
+		}
+	}
+
+	private void handleSnitchChatMsg(String msg) {
+		if (msg.contains("*") && msg.contains("snitch at")) {
+			// parseSnitch(msg)
+		} else if (msg.contains("Used") || msg.contains("Block Break") || msg.contains("Block Place")) {
+			// render block place/break/use
+			parseBlock(msg);
+		} else if (msg.contains("Entry")) {
+			if (snitchReport)
+				parseEntry(msg);
+		} else if (msg.contains("Snitch Log for") || msg.contains("Page 1 is empty for snitch")) {
+			// export jainfo to csv
+			try {
+				String name = parseSnitchName(msg);
+				if (SVPlayerHandler.updateSnitchName) {
+					SVPlayerHandler.updateSnitchName = false; // done!
+				}
+				SVFileIOHandler.saveList();
+
+				if (snitchReport) {
+					if (snitchReportName.equals("")) {
+						snitchReportName = name;
+					}
+					if (!snitchReportName.equals(name)) {
+						snitchReport = false;
+						SVFileIOHandler.saveSnitchReport(snitchReportName);
+						snitchReportName = "";
+					}
+				}
+			} catch (Exception e) {
+				logger.error("Exception encountered while parsing snitch logs", e);
+
+				try {
+					Minecraft.getMinecraft().thePlayer.addChatMessage(new TextComponentString("Check logs; " +
+							"non-fatal error while parsing snitch logs"));
+				} catch (Exception f) {
+					logger.error("Couldn't notify player of error", f);
+				}
+			}
+		} else if (msg.contains("Unknown command") || msg.contains(" is empty") || msg.contains("You do not own any snitches nearby!")) {
+			jalistIndex = 1;
+			jainfoIndex = 1;
+			updateSnitchList = false;
+			if (snitchReport) {
+				snitchReport = false;
+				SVFileIOHandler.saveSnitchReport(snitchReportName);
+				snitchReportName = "";
+			}
+		} else if (msg.contains("TPS from last 1m, 5m, 15m:")) {
+			ParseTPS(msg);
 		}
 	}
 
@@ -188,8 +249,9 @@ public class SVChatHandler {
 				cullTime = Double.parseDouble(match.group(5));
 			}
 			String ctGroup = match.group(6);
+			String snitchName = match.group(7);
 
-			Snitch n = new Snitch(worldName, worldUUID, x, y, z, cullTime, ctGroup, null);
+			Snitch n = new Snitch(worldName, worldUUID, x, y, z, cullTime, ctGroup, snitchName);
 
 			// TODO: just use a sorted collection, this is gross.
 			int index = Collections.binarySearch(SV.instance.snitchList, n);
